42bafc42e408d2faa26710f272a14d28ed6484d0
kennzuki <kingsyken@gmail.com>
2024-04-28 22:29:13 +0300

    making a better ui for results

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const dataForm=document.querySelector('#dataForm')
 const cityInput = document.querySelector('#city')
 const display = document.querySelector('#display')
-const place='Nairobi'
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=a9d6f8d2e6cb7ba0fd3cf5c5fa3a4b45`;
+const city = 'Miami'
+
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a9d6f8d2e6cb7ba0fd3cf5c5fa3a4b45`;
 
 
 
@@ -16,13 +17,18 @@ async function getWeather() {
         console.log(data); // Optional: log the data to see the structure
 
         const weatherInfo = `
+              <p>Country: ${data.sys.country}</p>
             <p >City: ${data.name}</p>
             <p>Temperature: ${(data.main.temp-273.15).toFixed(1)}°C</p>
             <p>Description: ${data.weather[0].description}</p>
+            <p>Wind Speed: ${data.wind.speed} mph</p>
+            <p>Humidity:${data.main.humidity}% </p>
         `;
         display.innerHTML = weatherInfo;
     } catch (error) {
+        const displayError=`<p >Error couldnt get city</p>`
         console.error('Error fetching weather data:', error);
+        display.innerHTML=displayError
     }
 }
 
